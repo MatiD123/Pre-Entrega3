5601eaa61298353625a14eee3b1694d664074be1
Matias Diano <matidiano4@gmail.com>
2023-04-20 11:26:48 -0300

    first commit

---
+++ shopping/data/stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Mesa",
+        precio: 4000,
+        desc: "La mejor mesa",
+        imagen: './public/images/mesa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Silla",
+        precio: 3000,
+        desc: "La verdadera silla",
+        imagen: './public/images/silla.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Sillón",
+        precio: 8000,
+        desc: "El sillón mas comodo",
+        imagen: './public/images/sillon.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Puerta",
+        precio: 2000,
+        desc: "En lo simple esta lo bonito!",
+        imagen: './public/images/puerta.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Cuadro",
+        precio: 1200,
+        desc: "El mejor cuadro para tu casa",
+        imagen: './public/images/cuadro.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Alacena",
+        precio: 5000,
+        desc: "Alacena que convina con todo",
+        imagen: './public/images/alacena.jpg',
+        cantidad:1
+    }
+];
---
+++ shopping/js/modal.js
@@ -0,0 +1,29 @@
+const modalContenedor = document.querySelector('.modal-contenedor');
+const abrirCarrito = document.getElementById('cesta-carrito');
+const cerrarCarrito = document.getElementById('btn-cerrar-carrito');
+const modalCarrito = document.querySelector('.modal-carrito')
+const procederCompra = document.getElementById('btn-proceder-compra')
+
+abrirCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+cerrarCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+modalContenedor.addEventListener('click', () => {
+    cerrarCarrito.click()
+});
+
+modalCarrito.addEventListener('click', (e) => {
+    e.stopPropagation()
+
+    if (e.target.classList.contains('boton-eliminar')) {
+        eliminarProductoCarrito(e.target.value)
+    }
+})
+
+procederCompra.addEventListener('click', () => {
+    
+})
